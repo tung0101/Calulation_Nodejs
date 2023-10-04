2aa5ea0b38425e81fefb4ff5d5348d871ed8c66e
Nguyễn Doãn Tùng <95358274+tung0101@users.noreply.github.com>
2023-10-04 11:50:45 +0700

    update lich.js

---
+++ models/lich.js
@@ -0,0 +1,40 @@
+const db = require('./db'); // Kết nối đến cơ sở dữ liệu
+
+class Lich {
+  constructor(thu, cong_viec, gio, ngay, ghi_chu) {
+    this.thu = thu;
+    this.cong_viec = cong_viec;
+    this.gio = gio;
+    this.ngay = ngay;
+    this.ghi_chu = ghi_chu;
+  }
+
+  static create(newEvent, result) {
+    db.query(
+      'INSERT INTO lich (thu, cong_viec, gio, ngay, ghi_chu) VALUES (?, ?, ?, ?, ?)',
+      [newEvent.thu, newEvent.cong_viec, newEvent.gio, newEvent.ngay, newEvent.ghi_chu],
+      (error, results) => {
+        if (error) {
+          console.error('Lỗi khi thêm sự kiện vào lịch: ', error);
+          result(error, null);
+          return;
+        }
+        console.log('Thêm sự kiện vào lịch thành công:', { id: results.insertId, ...newEvent });
+        result(null, { id: results.insertId, ...newEvent });
+      }
+    );
+  }
+
+  static getAll(result) {
+    db.query('SELECT * FROM lich', (error, results) => {
+      if (error) {
+        console.error('Lỗi khi lấy danh sách sự kiện từ lịch: ', error);
+        result(error, null);
+        return;
+      }
+      result(null, results);
+    });
+  }
+}
+
+module.exports = Lich;
