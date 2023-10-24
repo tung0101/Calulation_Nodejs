5df1ade8a15657dad37a58df2d4bed70021e7cd1
Nguyễn Doãn Tùng <95358274+tung0101@users.noreply.github.com>
2023-10-24 10:09:47 +0700

    final.js

---
+++ models/final.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root', 
+  password: '',
+  database: 'final', 
+});
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database: ' + err.stack);
+    return;
+  }
+  console.log('Connected to the database as id ' + db.threadId);
+});
+module.exports = db;
